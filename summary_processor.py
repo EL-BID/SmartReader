851524ec3d761c915959eaa126c64cddc6893559
Daniela Collaguazo <dcct84@gmail.com>
2018-07-31 16:27:29 -0400

    updates

--- summary_processor.py
+++ summary_processor.py
@@ -24,17 +24,12 @@ def updateJobStatus(jobid, status):
     )
 
 def run_job(job):
-    #print('run_job from summary_processor')
     jobid = ""
     for document in job:
         try:
             jobid = document["_id"]
             updateJobStatus(jobid, "Processing")
-            #print("Processing")
             output_json = create_summary(document["file_path"], document["model_file_name"])
-            print ("*******************************")
-            print (output_json)
-            print ("------------------------------")
             convert_txt_html(output_json)
             if not os.path.isdir('Summaries'):
                 os.mkdir('Summaries')

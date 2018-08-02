ece069b734d2246a85c8e2c6d09a8395a9f96c83
Daniela Collaguazo <dcct84@gmail.com>
2018-08-02 12:52:46 -0400

    debugging

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ from gensim.summarization import keywords
 from gensim.summarization import summarize
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.nlp.stemmers import Stemmer
+from sumy.summarizers.edmundson import EdmundsonSummarizer
+
 
 stemmer = Stemmer("english")
 summarizer = Summarizer(stemmer)

a8bbc7e730ed5d8509499aca6c6e68d78864be1c
Daniela Collaguazo <dcct84@gmail.com>
2018-08-02 12:44:19 -0400

    debugging

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from gensim.summarization import keywords
 from gensim.summarization import summarize
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+summarizer = Summarizer(stemmer)
 
 for sentence in summarizer(parser.document,1):
 	print(sentence)

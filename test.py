4e07210ec6d91ef8db1f5a18bc6adcdf277aa086
Daniela Collaguazo <dcct84@gmail.com>
2018-08-02 12:53:46 -0400

    debugging

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from gensim.summarization import summarize
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.summarizers.edmundson import EdmundsonSummarizer
-
+from sumy.parsers.html import HtmlParser
 
 stemmer = Stemmer("english")
 summarizer = Summarizer(stemmer)

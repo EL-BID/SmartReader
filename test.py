5c02ba288b628257a1b2dff4bccae68d727001ed
Daniela Collaguazo <dcct84@gmail.com>
2018-08-02 12:47:27 -0400

    debugging

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from gensim.summarization import keywords
 from gensim.summarization import summarize
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+
+stemmer = Stemmer("english")
 summarizer = Summarizer(stemmer)
 
 for sentence in summarizer(parser.document,1):

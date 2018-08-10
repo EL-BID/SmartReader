9c642e3ca0efec1d85b51247619cf3fb09690a27
Seaford Bacchas <richybacchas0911@gmail.com>
2018-08-10 14:56:31 -0400

    Documenting

--- src/process.py
+++ src/process.py
@@ -102,12 +102,14 @@ def create_summary(dataset_location, model_name):
 		all_entities_type = defaultdict(lambda:0) #creating dictionary
 		summary_points = []
 		paragraphs = topic["paragraphs"]
-		for p in paragraphs[0:10]: # iterating through the first 50 paragraphs
-			print("XXXXXXXXXXXXXXXXXX")
-			print(p)
+		for p in paragraphs[0:3]: # iterating through the 50 most relevant paragraphs
 			try:
 				full_text = p["para"].text
+				print("XXXXXXXXXXXXXXXXXXXXXX")
+				print(full_text)
 				sentences = sent_tokenize(full_text)
+				print("*-*-*-*-*-*-*-*-*-*-*")
+				print(sentences)
 				summary = get_summary(full_text, 1, len(sentences))[0]
 				original_sentence = summary
 				summary_index = sentences.index(summary)

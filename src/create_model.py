50e7807787f21e83240f98e38c2a533b6effac57
Daniela Collaguazo <dcct84@gmail.com>
2018-09-06 15:42:46 -0400

    updates

--- src/create_model.py
+++ src/create_model.py
@@ -36,6 +36,7 @@ def create_and_save_model(subtopics, output_file):
 			data.append( {"topic":subtopic_names[i], "keywords":features_with_weights, "vectorizer":vec, "feature_indices":feature_indices} )
 	pickle.dump( data, open(output_file, "wb") )
 	print("This is the model", data)
+	temp_s = str(data)
 	d = json.dumps(data)
 	print(type(d))
 	global gvec

bd549ce2f9714123b9c063dfcc31604d8e939d98
Daniela Collaguazo <dcct84@gmail.com>
2019-04-19 13:29:18 -0400

    updt

--- src/create_model.py
+++ src/create_model.py
@@ -75,10 +75,10 @@ def create_and_save_model(subtopics, output_file):
 				"keywords":features_with_weights, \
 				"vectorizer":vec, \
 				"feature_indices":feature_indices})
-			print('*******************************')
-			print(type(data))
+			#print('*******************************')
+			#print(type(data))
 			# print(str(data))
-	print(data)
+	print(data.shape)
 			# output["keywords"] = features_with_weights
 			# print(features_with_weights)
 			# print(type(features_with_weights))

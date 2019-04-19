ce5b63bf09e1be56f7ee94230b93ab2be50ba462
Daniela Collaguazo <dcct84@gmail.com>
2019-04-19 16:19:59 -0400

    u

--- src/create_model.py
+++ src/create_model.py
@@ -76,11 +76,13 @@ def create_and_save_model(subtopics, output_file):
 				"keywords":features_with_weights, \
 				"vectorizer":vec, \
 				"feature_indices":feature_indices})
+			print('XXXXXXXXXXXXXXXXXXX')
+			print(data)
 			
 			# model_to_print is of type list
-			model_to_print.append({"subtopic":subtopic_names[i], \
-				"keywords":features_with_weights, \
-				"feature_indices":feature_indices})
+			# model_to_print.append({"subtopic":subtopic_names[i], \
+			# 	"keywords":features_with_weights, \
+			# 	"feature_indices":feature_indices})
 			#print('XXXXXXXXXXXXXXXXXXXXXXXXXXXX')
 			#print(type(model_to_print))
 			#for i in model_to_print:

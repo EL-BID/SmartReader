59596dc75b216c2dd30ab67909f73c40189c76ce
Daniela Collaguazo <dcct84@gmail.com>
2019-04-19 11:59:41 -0400

    upds

--- src/create_model.py
+++ src/create_model.py
@@ -77,10 +77,7 @@ def create_and_save_model(subtopics, output_file):
 				"feature_indices":feature_indices})
 			print('*******************************')
 			print(type(data))
-			print(str(data))
-			#with open('./log/data.txt','a',encoding='utf-8'):
-				#f.write('Hello')
-	#print(len(data))
+			# print(str(data))
 	print(data)
 			# output["keywords"] = features_with_weights
 			# print(features_with_weights)
